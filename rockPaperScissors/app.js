d95f06552c084e836ec54b29ce85c8b94ab2a688
weihongchen97 <weihongchen97@gmail.com>
2024-02-21 17:20:44 -0800

    Modified the original assignment to have buttons instead of prompts

--- rockPaperScissors/app.js
+++ rockPaperScissors/app.js
@@ -1,51 +1,61 @@
-//Initial Single Game Code
 function computerMove() {
     const computerPoss = ['rock', 'paper', 'scissors'];
     const randomC = Math.floor(Math.random() * computerPoss.length);
     return computerPoss[randomC];
 }
 
+let playerScore = 0;
+let computerScore = 0;
+const scoreboard = document.querySelector("#scoreboard");
+const choices = document.querySelector("#choices")
+
 function throwHand(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
-        alert("It's a draw!!!")
+        choices.innerText = (`You threw ${playerSelection} and they threw ${computerSelection}`);
+        scoreboard.innerText = (`It's a draw! Current score - Player: ${playerScore}, Computer: ${computerScore}`);
     } else if (
     (playerSelection === 'rock' && computerSelection === 'scissors') ||
     (playerSelection === 'paper' && computerSelection === 'rock') ||
     (playerSelection === 'scissors' && computerSelection === 'paper')
     ){
-        alert("You won!!!");
-        console.log("player won a game!");
-        return "player"
+        playerScore++;
+        choices.innerText = (`You threw ${playerSelection} and they threw ${computerSelection}`);
+        scoreboard.innerText = (`Current score - Player: ${playerScore}, Computer: ${computerScore}`);
+        checkScorecard();
     } else {
-        console.log("you lost!")
-        return "computer"
+        computerScore++;
+        choices.innerText = (`You threw ${playerSelection} and they threw ${computerSelection}`);
+        scoreboard.innerText = (`Current score - Player: ${playerScore}, Computer: ${computerScore}`);
+        checkScorecard();
     } 
 }
 
-
-const computerSelection = computerMove();
-//now structure the game to be 5 games
-function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Throw your hand! Rock, paper, or scissors?").toLowerCase();
-        while (!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors')) {
-            playerSelection = prompt("PLEASE TYPE IN YOUR CHOICE! ROCK, PAPER, OR SCISSORS?").toLowerCase();
-        } 
-        
-        const scoreCard = throwHand(playerSelection, computerMove())
-        if (scoreCard === 'player') {
-            playerScore++;
-            console.log(`You have won a point! ${playerSelection} beats ${computerSelection}`)
-        } else if (scoreCard === "computer"){
-            computerScore++;
-            console.log(`Your oponent has gained a point! ${computerSelection} beats ${playerSelection}!`)
-        } else {
-            console.log("tie round!")
-        }
-    }
-    console.log(`Final Scores - Player: ${playerScore}, Computer: ${computerScore}`);
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
 }
-playGame();
+function checkScorecard() {
+    if(playerScore === 5) {
+        scoreboard.innerText = (`Congratulations! You won!!! Final Scores - Player: ${playerScore}, Computer: ${computerScore}`);
+        resetGame();
+    } else if(computerScore === 5) {
+        scoreboard.innerText = (`DEFEAT!!! Final Scores - Player: ${playerScore}, Computer: ${computerScore}`);
+        resetGame();
+    }
+}
+
+function playGame(playerSelection) {
+    const computerSelection = computerMove();
+    throwHand(playerSelection, computerSelection);
+}
+  
+
+const throwRock = document.querySelector("#rock");
+const throwPaper = document.querySelector("#paper");
+const throwScissors = document.querySelector("#scissors");
+
+throwRock.addEventListener("click", () => playGame("rock"));
+throwPaper.addEventListener("click", () => playGame("paper"));
+throwScissors.addEventListener("click", () => playGame("scissors"));
+
+
